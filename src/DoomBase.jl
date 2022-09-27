2f88f26220542f71829c53a8f28d5fc2e5fc9738
Sam Buercklin <sam.buercklin@gmail.com>
2022-09-26 22:08:41 -0400

    initial commit with PLAYPAL

---
+++ src/DoomBase.jl
@@ -0,0 +1,8 @@
+module DoomBase
+
+using ColorTypes: RGB, N0f8
+using StaticArrays
+
+include("PLAYPAL.jl")
+
+end # module DoomBase
---
+++ src/PLAYPAL.jl
@@ -0,0 +1,26 @@
+const DOOMCOLOR = RGB{N0f8}
+
+function DOOMCOLOR(r::T, g::T, b::T) where {T <: UInt8}
+    _r = reinterpret(N0f8, r)
+    _g = reinterpret(N0f8, g)
+    _b = reinterpret(N0f8, b)
+    DOOMCOLOR(_r, _g, _b)
+end
+
+struct Palette
+    colors::SizedVector{256, DOOMCOLOR, Vector{DOOMCOLOR}}
+end
+function Palette(colors::Vector{UInt8})
+    ncolors = 256
+    color_vec = DOOMCOLOR[]
+    for n in 1:ncolors
+        r, g, b = colors[(1:3) .+ (n-1) * 3]
+        push!(color_vec, DOOMCOLOR(r, g, b))
+    end
+
+    return Palette(SizedVector{ncolors}(color_vec))
+end
+
+struct PLAYPAL
+    palettes::NTuple{14, Palette}
+end
